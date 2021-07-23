35f3a471e7eb4d73b44fef4037facecf55d9b801
ssn6dev <nakano2uka@gmail.com>
2021-07-23 22:03:52 +0900

    テキストからdocxに変換する機能追加

--- run.py
+++ run.py
@@ -1,21 +1,51 @@
-from datetime import datetime, date
+from docx import Document
+from docx.oxml.ns import qn
+
+import os
 import pathlib
-import docx
+from datetime import datetime, date
+
+dir_path = pathlib.Path(__file__).parent
+docx_path = dir_path / "all.docx"
+template_path = dir_path / "template.docx"
+txt_path = dir_path / "all.txt"
+csv_path = dir_path / "statistic.csv"
 
 yyyymmdd = date.today().strftime("%Y/%m/%d")
-docx_path = pathlib.Path(__file__).parent / "all.docx" 
-txt_path = pathlib.Path(__file__).parent / "all.txt"
-csv_path = pathlib.Path(__file__).parent / "statistic.csv"
 
-doc = docx.Document(docx_path)
-num_of_char = 0
-with open(txt_path, "w", encoding='utf-8') as f:
-  for para in doc.paragraphs:
-    encoded_text = para.text
-    print(para.text, file=f)
+def convert_doc_to_txt():
+  document = Document(docx_path)
+  with open(txt_path, "w", encoding='utf-8') as f:
+    for para in document.paragraphs:
+      print(para.text, file=f)
+
+def convert_txt_to_doc():
+  document = Document(template_path)
+  with open(txt_path, "r", encoding='utf-8') as f:
+    txt_content = f.read().split("\n")
+    for para_content in txt_content:
+      para = document.add_paragraph()
+      run = para.add_run(para_content)
+      run.font.name = "源暎こぶり明朝 v6"
+      run._element.rPr.rFonts.set(qn('w:eastAsia'), run.font.name)
+    document.save(docx_path)
+
+def get_num_of_char():
+  num_of_char = 0
+  document = Document(docx_path)
+  for para in document.paragraphs:
     num_of_char += len(para.text)
     "{:,}".format(num_of_char)
+  with open(csv_path, "a") as f:
+    add_content = f'{yyyymmdd},{num_of_char}'
+    print(add_content, file=f)
 
-with open(csv_path, "a") as f:
-  add_content = f'{yyyymmdd},{num_of_char}'
-  print(add_content, file=f)
+txt_mtime = os.path.getmtime(txt_path)
+docx_mtime = os.path.getmtime(docx_path)
+
+if txt_mtime >= docx_mtime:
+  convert_txt_to_doc()
+else:
+  convert_doc_to_txt()
+
+get_num_of_char()
