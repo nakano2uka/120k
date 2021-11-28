7c188fbe7400d6603c2cd97da0ee37c6ce489e50
nakano2uka <nakano2uka@gmail.com>
2021-11-29 06:19:30 +0900

    整理

--- run.py
+++ run.py
@@ -9,17 +9,26 @@ dir_path = pathlib.Path(__file__).parent
 docx_path = dir_path / "all.docx"
 template_path = dir_path / "template.docx"
 txt_path = dir_path / "all.txt"
+txt_sep_list = dir_path/"分割"
+txt_sep_list = list(txt_sep_list.glob("*"))
 csv_path = dir_path / "statistic.csv"
 
 yyyymmdd = date.today().strftime("%Y/%m/%d")
 
-def convert_doc_to_txt():
+def convert_to_txt():
   document = Document(docx_path)
   with open(txt_path, "w", encoding='utf-8') as f:
     for para in document.paragraphs:
       print(para.text, file=f)
 
-def convert_txt_to_doc():
+def convert_to_all_txt():
+  with open(txt_path, "w", encoding='utf-8') as all:
+    for i in txt_sep_list:
+      with open(i,"r",encoding='utf-8') as sep:
+        data = sep.read()
+        print(data, file=all)
+
+def convert_to_doc():
   document = Document(template_path)
   with open(txt_path, "r", encoding='utf-8') as f:
     txt_content = f.read().split("\n")
@@ -43,9 +52,15 @@ def get_num_of_char():
 txt_mtime = os.path.getmtime(txt_path)
 docx_mtime = os.path.getmtime(docx_path)
 
+'''
 if txt_mtime >= docx_mtime:
-  convert_txt_to_doc()
+  #convert_to_doc()
 else:
-  convert_doc_to_txt()
+  #convert_to_txt()
+'''
+
+#convert_to_txt()
+#convert_to_all_txt()
+#convert_to_doc()
 
 get_num_of_char()
